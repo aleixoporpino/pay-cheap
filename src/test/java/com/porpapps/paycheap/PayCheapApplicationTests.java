ddf55d44fea48aa7eba34fe245c9c66d08667360
Jose Aleixo Araujo Porpino Filho <aleixoporpino@gmail.com>
2022-11-30 19:39:43 -0500

    Initial commit

---
+++ src/test/java/com/porpapps/paycheap/PayCheapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porpapps.paycheap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayCheapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
